7feee23150f1532c6453323c2ed0c8fd969baaf0
rbizimungu <143827106+rbizimungu@users.noreply.github.com>
2025-02-07 02:10:19 +0000

    implemented count_subarrays_with_sum function

--- p3.py
+++ p3.py
@@ -1,2 +1,12 @@
 def count_subarrays_with_sum(arr, sum):
-    pass
+    initial_sum = {0:1}
+    result = 0
+    current_sum = 0
+    arr = arr[::-1]
+
+    for val in arr:
+        current_sum += val
+        if (current_sum - sum) in initial_sum:
+            result += initial_sum[current_sum - sum]
+        initial_sum[current_sum] = initial_sum.get(current_sum, 0) + 1
+    return result
